2da9eb62b7fb16051e0a6fa4791cba7d1b6391e3
wLink69 <linhwhite98@gmail.com>
2020-03-23 18:34:52 +0700

    seeder

--- src/main/java/com/perfume/repository/UserRepository.java
+++ src/main/java/com/perfume/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.perfume.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+    User findByStatus(int status);
     Boolean existsByUsername(String username);
 }
