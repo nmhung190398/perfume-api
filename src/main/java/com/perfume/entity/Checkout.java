640fc0d0d337e30c27ec0a924d4a8bc35d3fc376
wLink69 <linhwhite98@gmail.com>
2020-03-24 18:02:31 +0700

    update db

---
+++ src/main/java/com/perfume/entity/Checkout.java
@@ -0,0 +1,23 @@
+package com.perfume.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Builder
+@Entity
+public class Checkout extends BaseEntity {
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    public User user;
+
+    @ManyToOne
+    @JoinColumn(name = "checkout_id")
+    public Coupon coupon;
+
+    @OneToMany(mappedBy = "checkout")
+    public List<CheckoutItem> checkoutItems;
+}
---
+++ src/main/java/com/perfume/entity/CheckoutItem.java
@@ -0,0 +1,31 @@
+package com.perfume.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@Entity
+public class CheckoutItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(name = "user_id", insertable = false, updatable = false)
+    public Long userId;
+
+    @Column(name = "product_id", insertable = false, updatable = false)
+    public Long productId;
+
+    public int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    public Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "checkout_id")
+    public Checkout checkout;
+}
