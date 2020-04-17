641774c9d97ca9dbe14e8b116f0542c911de7ce3
hung.nguyen <hung.nguyen@evotek.vn>
2020-04-17 16:21:52 +0700

    update

--- src/main/java/com/perfume/config/AuditorAwareImpl.java
+++ src/main/java/com/perfume/config/AuditorAwareImpl.java
@@ -12,12 +12,13 @@ public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
     public Optional<String> getCurrentAuditor() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return null;
-        }
+
         String username = "system";
         try{
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication == null || !authentication.isAuthenticated()) {
+                return null;
+            }
             username = ((UserDetails) authentication.getPrincipal()).getUsername();
         }catch (Exception e){
             e.printStackTrace();
